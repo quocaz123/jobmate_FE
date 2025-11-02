9f89a106511370ce6abd2e2204a11a81cfe502d0
Tamtm1 <tampro112212@gmail.com>
2025-11-02 18:35:06 +0700

    u

--- src/pages/employer/EmployerPage.jsx
+++ src/pages/employer/EmployerPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import DashboardLayout from '../layouts/DashboardLayout';
-import { getUserInfo } from '../utils/userUtils';
-import { employerMenuItems } from '../utils/menuConfig';
+import DashboardLayout from '../../layouts/DashboardLayout';
+import { getUserInfo } from '../../utils/userUtils';
+import { employerMenuItems } from '../../utils/menuConfig';
 
 // Component cho Overview
 const EmployerOverview = () => {
