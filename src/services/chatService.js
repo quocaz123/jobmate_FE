4c1500b6426fc7042a02c0e69dbd6ffa92389774
quocaz123 <quocthangbinh234@gmail.com>
2025-11-04 14:13:24 +0700

    Save local changes before merge

---
+++ src/services/chatService.js
@@ -0,0 +1,25 @@
+import httpClient from "../configurations/httpClient";
+import { CHAT } from "../configurations/configuration";
+
+export  const getMyConversations = async () => {
+    return await httpClient.get(CHAT.GET_MY_CONVERSATIONS);
+}
+
+export const createConversation = async (data) => {
+    return await httpClient.post(CHAT.CREATE_VERSATION, {
+        type: "DIRECT",
+        participantIds: data.participantIds
+    });
+}
+
+export const getMessagesOfConversation = async (conversationId) => {
+    return await httpClient.get(`${CHAT.GET_MESSAGES_OF_CONVERSATION}?conversationId=${conversationId}`);
+}
+
+export const createMessage = async (data) => {
+    return await httpClient.post(CHAT.CREATE_MESSAGE, {
+        conversationId: data.conversationId,
+        message: data.message
+    });
+}
+
