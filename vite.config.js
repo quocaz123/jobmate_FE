843f4b60276ce06be6a4773ababdfbf51795e70d
quocaz123 <quocthangbinh234@gmail.com>
2025-12-21 22:29:08 +0700

    Update import statements and enhance Vite configuration for better module resolution

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+  },
   server: {
     port: 5173,
     strictPort: true,
